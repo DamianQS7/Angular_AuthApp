148623f2cf72ca600d1c1bc23dee135f9fc6ab25
DamianQuezada <damianquezada.s@gmail.com>
2024-05-07 12:54:27 -0300

    Added Guards

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  private fb: FormBuilder = inject(FormBuilder);
+
+  public registerForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
+  });
+
+  public register(): void {
+    const { name, email, password } = this.registerForm.value;
+    
+  }
 }
